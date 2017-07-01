d95a54a65db56a9b5db3567c377d80256ea319a3
S Mautner <mautner@cs.uni-freiburg.de>
2017-07-01 18:17:18 +0200

    hayaku!

--- graphlearn/lsgg_extensions/some_neighbors.py
+++ graphlearn/lsgg_extensions/some_neighbors.py
@@ -2,11 +2,17 @@ import random
 import graphlearn2.graphlearn.lsgg as base_grammar
 
 class lsgg(base_grammar.lsgg):
-    def some_neighbors(self,graph,num_neighbors):
-        self.label_preprocessing(graph)
+
+
+    def nodes_in_random_order(self,graph):
         nodes=[n for n in graph.nodes() if 'edge' not in graph.node[n]]
         random.shuffle(nodes)
-        for n in nodes:
+        return nodes
+
+
+    def some_neighbors(self,graph,num_neighbors):
+        self.label_preprocessing(graph)
+        for n in self.nodes_in_random_order(graph):
             for orig_cip in self._rooted_decompose(graph,n):
                 for graph2 in self._neighbors_given_orig_cips(graph, [orig_cip]):
                     if num_neighbors > 0:
@@ -14,7 +20,6 @@ class lsgg(base_grammar.lsgg):
                         yield graph2
                     else:
                         raise StopIteration
-
 
 
 
